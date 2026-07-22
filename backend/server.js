2cdb6ffb7e808bdb1ae8dd30180ea46ff7d073d9
Srivatsankith <srivatsankith.2005@gmail.com>
2026-07-22 15:15:38 +0530

    Changes done in admin panel profile

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.static(path.join(__dirname, "..")));
 
